78594499406bac923615c764dbcf1a7dfe4b0391
Lilli Reilly <lilli.reilly@gmail.com>
2022-04-03 20:35:52 +0200

    Build

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 import Header from "./components/header/Header"
 import Nav from "./components/nav/Nav"
 import About from "./components/about/About"
-import Services from './components/services/Services'
 import Portfolio from './components/portfolio/Portfolio'
-import Testimonials from './components/testimonials/Testimonials'
 import Contacts from './components/contacts/Contacts'
 import Footer from './components/footer/footer'
 
